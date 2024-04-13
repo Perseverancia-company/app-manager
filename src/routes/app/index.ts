4886fd64e42ae245e67a0d6ad349a2d2de41b501
Felix <fedecraft3312@gmail.com>
2024-04-13 10:20:44 -0300

    Some logging messages

--- src/routes/app/index.ts
+++ src/routes/app/index.ts
@@ -8,11 +8,14 @@ const appRouter = express.Router();
 
 appRouter.post('/', (req, res) => {
     try {
+        console.log(`[GET] /app`);
+        
         // Read apps and get their information
         const {
             path 
         } = req.body;
         
+        console.log(`Path: `, path);
         const app = new AppData(path);
         return res.status(200)
             .json({
@@ -24,6 +27,9 @@ appRouter.post('/', (req, res) => {
             });
     } catch(err: any) {
         console.error(err);
+        // This can fail often
+        console.log(`Error: The given app surely doesn't have 'package.json' file`);
+        
         return res.status(500).json({
             messages: [{
                 error: true,
--- src/routes/apps/index.ts
+++ src/routes/apps/index.ts
@@ -8,6 +8,7 @@ const appsRouter = express.Router();
 
 appsRouter.get('/', (req, res) => {
     try {
+        console.log(`[GET] /apps`);
         // Read apps and get their information
         
         const apps = new Apps();
